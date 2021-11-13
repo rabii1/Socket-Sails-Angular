920c4ea31a8dfd1295bbed5f3dcd513bb38e006e
R.ROGUI <rabii.rogui1@gmail.com>
2021-11-13 12:51:41 +0100

    update 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SailsClient} from '@aloreljs/ngx-sails';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'Socket-Sails-Angular';
+  daten= "Getting info en cours ..."
+  listAction:any=[];
+
+  constructor ( private readonly sails: SailsClient) {
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  //  this._sailsService.connect("http://localhost:1337");
+    // or
+   
+ 
+
+this.sails.io.connect()
+this.sails.on('connect').subscribe((response:any) => {
+ 
+ 
+  setTimeout(() => {
+    this.daten="LoggedIn at : "+new Date().toUTCString()
+
+  }, 3500);
+
+ 
+
+  this.sails.get('/notifications/subscribeAdmins').subscribe((data:any)=>{
+    console.log("service suscribe")
+    console.log(data)
+    this.sails.on('administrators').subscribe((response1:any) => {
+      this.listAction.push({pers:response1.pers,datex:response1.datex,action:response1.action})
+      console.log("new message")
+      console.log(response1)
+    });
+
+
+  });
+
+});
+  }
+
+   
+  public listenForEvent(): void {
+    this.sails.on('eventName').subscribe((response:any) => {});
+  }
+ 
+  public makeRequest(): void {
+    this.sails.get('url').subscribe();
+    this.sails.post('url', {foo: 'bar'}).subscribe((response:any) => {});
+  }
+ 
+  public emitEventWithoutAResponse(): void {
+    this.sails.emit('eventName', 'arg1', 'arg2');
+  }
+ 
+  public emitEventWithAResponse(): void {
+    this.sails.emitAndWait('eventName', 'arg1', 'arg2').subscribe((response:any)  => {})
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NGX_SAILS_CONFIG, NgxSailsConfig} from '@aloreljs/ngx-sails';
+import { CommonModule } from "@angular/common";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const config: NgxSailsConfig = {
+  uri: 'http://192.168.1.110:1439',
+  // socket.io options
+  options: {
+         transports: ['polling', 'websocket'],
+       autoConnect:false,
+       
+  },
+  // default headers
+  headers: {
+  
+  }
+}
+
 
 @NgModule({
   declarations: [
@@ -10,9 +27,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,CommonModule
+    
   ],
-  providers: [],
+  providers: [ {
+    provide: NGX_SAILS_CONFIG,
+    useValue: config
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
